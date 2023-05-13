05bbd83865015e278bcb96d55cf2b58bfd3e1b22
sanat <you@exampl.com>
2023-05-12 21:13:06 -0400

    5/12/23

--- app/endpoints/urls.py
+++ app/endpoints/urls.py
@@ -2,10 +2,14 @@
 Author Sanatjon Burkhanov
 github: posesred
 """
-from typing import Iterable
 
 
 class APIPrefix:
+    """
+    all this does is add a prefix or like order pre means before fix is like syntax
+    ex:/api/operations/create
+    ex:/api/operations/delete
+    """
     include = [
-        'operation'
+        'operations'
     ]
